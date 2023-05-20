8c857228bf32a48adf5cf4614e037ee272d39335
Becky Smith <rebkwok@gmail.com>
2023-05-20 09:49:00 +0100

    Better logging format

--- bot.py
+++ bot.py
@@ -121,6 +121,6 @@ class Twooter:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
     twooter = Twooter()
     twooter.run()
